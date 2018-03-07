096de3eec9150a71332c51f73a30178f398acc3a
Samuel SAYAG <samuel.sayag@clicktale.com>
2018-03-07 17:17:24 +0200

    add some beginning of implementation

--- src/main/java/com/erez/stuff/DeepCompare.java
+++ src/main/java/com/erez/stuff/DeepCompare.java
@@ -1,9 +1,52 @@
 package com.erez.stuff;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonPrimitive;
+
+import java.util.List;
+
 public class DeepCompare {
 
 
-    static public deepCompare()
+    static public List<FieldCompare> deepCompare(String json1,
+                                                 String json2) {
+        return null;
+    }
 
+
+    static public List<FieldCompare> deepCompare(JsonElement json1,
+                                                 JsonElement json2) {
+        return null;
+    }
+
+    static private List<FieldCompare> deepCompare(String path,
+                                                  List<FieldCompare> acc,
+                                                  String json1,
+                                                  String json2) {
+        return null;
+    }
+
+    static public FieldCompare compare(String path,
+                                       JsonPrimitive val1,
+                                       JsonPrimitive val2) {
+        return new FieldCompare(path,
+                val1.getAsString(),
+                val2.getAsString(),
+                val1.equals(val2));
+    }
+
+    static public List<FieldCompare> compare(String path,
+                                             JsonArray val1,
+                                             JsonArray val2) {
+        return null;
+    }
+
+    static public <T> List<FieldCompare> compare(String path,
+                                                 JsonNull jsonNull,
+                                                 T val2) {
+        return null;
+    }
 
 }
--- src/main/java/com/erez/stuff/FieldCompare.java
+++ src/main/java/com/erez/stuff/FieldCompare.java
@@ -1,9 +1,32 @@
 package com.erez.stuff;
 
 public class FieldCompare {
+    
+    public FieldCompare(String jsonPath, String val1, String val2, Boolean cmp) {
+        this.jsonPath = jsonPath;
+        this.val1 = val1;
+        this.val2 = val2;
+        this.cmp = cmp;
+    }
 
-    public String jsonPath = null;
-    public String val1 = null;
-    public String val2 = null;
-    public Boolean cmp = false;
+    String jsonPath = null;
+    String val1 = null;
+    String val2 = null;
+    Boolean cmp = false;
+
+    public String getJsonPath() {
+        return jsonPath;
+    }
+
+    public String getVal1() {
+        return val1;
+    }
+
+    public String getVal2() {
+        return val2;
+    }
+
+    public Boolean getCmp() {
+        return cmp;
+    }
 }
